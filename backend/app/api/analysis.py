03d2e61ff65f56720af1a18278824fd79af834ab
hari-hara-sudharsan <sudharsanj008gmail.com>
2026-06-22 14:31:51 +0530

    Push analysis.py

---
+++ backend/app/api/analysis.py
@@ -0,0 +1,39 @@
+from fastapi import APIRouter
+
+from app.schemas.wallet_schema import WalletRequest
+
+from app.services.wallet_analyzer import WalletAnalyzer
+
+from app.services.credit_engine import CreditEngine
+
+from app.services.credit_analyst import CreditAnalyst
+
+router = APIRouter(
+    prefix="/analysis",
+    tags=["Analysis"]
+)
+
+
+@router.post("/")
+def analyze_wallet(
+    request: WalletRequest
+):
+
+    analyzer = WalletAnalyzer()
+
+    credit_engine = CreditEngine()
+
+    credit_analyst = CreditAnalyst()
+
+    features = analyzer.analyze(
+        request.wallet
+    )
+
+    profile = credit_engine.calculate(
+        features
+    )
+
+    return credit_analyst.analyze(
+        features,
+        profile
+    )
