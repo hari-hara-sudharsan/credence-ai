4097f419cea5f09cc71a2607c9da668f25fcb5ab
hari-hara-sudharsan <sudharsanj008gmail.com>
2026-06-22 14:32:28 +0530

    Push main.py

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.api.wallet import router as wallet_router
 from app.api.credit import router as credit_router
+from app.api.analysis import router as analysis_router
 
 app = FastAPI(
     title="Credence AI",
@@ -9,3 +10,6 @@ app = FastAPI(
 
 app.include_router(wallet_router)
 app.include_router(credit_router)
+app.include_router(
+    analysis_router
+)
