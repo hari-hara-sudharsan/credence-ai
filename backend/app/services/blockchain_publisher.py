7ebd98739a9af5d7f9d386181369266f05d614e6
hari-hara-sudharsan <sudharsanj008gmail.com>
2026-06-22 22:54:30 +0530

    Add/Update backend/app/services/blockchain_publisher.py

---
+++ backend/app/services/blockchain_publisher.py
@@ -0,0 +1,98 @@
+import os
+
+from web3 import Web3
+
+from dotenv import load_dotenv
+
+# pyrefly: ignore [missing-import]
+from app.contracts.credit_registry_abi import (
+    CREDIT_REGISTRY_ABI
+)
+
+load_dotenv()
+
+
+class BlockchainPublisher:
+
+    def __init__(self):
+
+        self.w3 = Web3(
+            Web3.HTTPProvider(
+                os.getenv("HSK_RPC")
+            )
+        )
+
+        self.account = self.w3.eth.account.from_key(
+            os.getenv("PRIVATE_KEY")
+        )
+
+        self.contract = self.w3.eth.contract(
+            address=Web3.to_checksum_address(
+                os.getenv("CREDIT_REGISTRY")
+            ),
+            abi=CREDIT_REGISTRY_ABI
+        )
+
+    def publish_score(
+
+        self,
+
+        wallet,
+
+        score,
+
+        rating,
+
+        confidence
+
+    ):
+
+        nonce = self.w3.eth.get_transaction_count(
+            self.account.address
+        )
+
+        tx = (
+            self.contract.functions
+            .updateCreditProfile(
+                wallet,
+                score,
+                rating,
+                confidence
+            )
+            .build_transaction(
+                {
+                    "from":
+                    self.account.address,
+
+                    "nonce":
+                    nonce,
+
+                    "gas":
+                    300000,
+
+                    "gasPrice":
+                    self.w3.eth.gas_price
+                }
+            )
+        )
+
+        signed = (
+            self.account
+            .sign_transaction(tx)
+        )
+
+        tx_hash = (
+            self.w3.eth
+            .send_raw_transaction(
+                signed.raw_transaction
+            )
+        )
+
+        receipt = (
+            self.w3.eth
+            .wait_for_transaction_receipt(
+                tx_hash
+            )
+        )
+
+        return receipt.transactionHash.hex()
